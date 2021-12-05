603b0f6b0a0ceb670a2478f168528fbd718db665
pepe27 <83622952+pepe27@users.noreply.github.com>
2021-12-05 14:46:10 -0500

    css is not working..

--- register.php
+++ register.php
@@ -2,16 +2,38 @@
 //register-form.php
 include("includes/header.php");
 ?>
+<link rel="stylesheet" media="screen" href="css/form.css">
 <title>Registration Form</title>
 <h1>A journey of a thousand miles begins with a single step :</h1>
 
+<!-- <section id="regForm">
+    <form action="process-register.php" method="POST"> 
+    <label for="username">Username</label><br>
+    <input type="text" name="username" required> <br>
+    <label for="password">Password</label><br>
+    <input type="text" name="password" required> <br>
+    <label for="nickname">Nickname</label><br>
+    <input type="text" name="nickname"> <br>
+    <label for="email">Email</label><br>
+    <input type="text" name="email" required> <br>
+    <label for="dob">Dob</label><br>
+    <input type="text" name="dob" required> <br>
+    <input type="submit" name="submit">
+    </form>
+</section> -->
+
 <section id="regForm">
     <form action="process-register.php" method="POST"> 
-    <input type="text" name="username" placeholder="username">
-    <input type="text" name="password" placeholder="password">
-    <input type="text" name="nickname" placeholder="firstName">
-    <input type="text" name="email" placeholder="lastName">
-    <input type="text" name="dob" placeholder="dob">
+    <label for="username">Username</label>
+    <input type="text" name="username" required> 
+    <label for="password">Password</label>
+    <input type="text" name="password" required>
+    <label for="nickname">Nickname</label>
+    <input type="text" name="nickname">
+    <label for="email">Email</label>
+    <input type="text" name="email" required>
+    <label for="dob">Dob</label>
+    <input type="text" name="dob" required>
     <input type="submit" name="submit">
     </form>
 </section>
