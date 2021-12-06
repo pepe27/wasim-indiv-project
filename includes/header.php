cd60f03d5e03e6479f4b75895a5914510ddf8d8f
pepe27 <83622952+pepe27@users.noreply.github.com>
2021-12-05 22:12:56 -0500

    html gogo

--- includes/header.php
+++ includes/header.php
@@ -14,10 +14,14 @@
     <link rel="canonical" href="http://www.wisp-journal.com/" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" type="image/jpg" href="assets/logo.jpg"/>
+    <link rel="shortcut icon" type="image/jpg" href="assets/wisp-favicon.png"/>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Tangerine">
     <link rel="stylesheet" href="https://use.typekit.net/cqi5tsz.css"> 
-    <!-- dazzle unicase font -->
+    <!-- Dazzle Unicase font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300&family=Work+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <!-- Work Sans font -->
     <link rel="stylesheet" media="screen" href="css/form.css">
     <link rel="stylesheet" media="screen" href="css/base.css">
     <link rel="stylesheet" media="screen and (max-width:480px)" href="css/mobile.css">
@@ -27,7 +31,7 @@
 <body>
 <!-- style = "padding: 15em 2em 10em;"     -->
     <header>
-        <img src="assets/logo.jpg" alt="wisp logo">
+        <img src="assets/wisp-favicon.png" alt="wisp logo" id="logo">
         <nav>
             <ul>
                 <li><a href="about.php">About</a></li>
--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php 
-// index.php
+// login.php
 // login-in form 
 include("includes/header.php");
 ?>
