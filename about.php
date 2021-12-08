17dd658d866bef7423252a82a300f0beaad70b22
pepe27 <83622952+pepe27@users.noreply.github.com>
2021-12-07 21:03:10 -0500

    about page gets info from about table in DB

--- about.php
+++ about.php
@@ -5,10 +5,32 @@ include("includes/header.php");?>
 <title>About Wisp</title>
 
 <h1>Brevity is the soul of wit</h1>
-<section id="about">
+<!-- <section id="about">
 <p>Need a place to jot out all the thoughts in your Head? Too stressed but don’t want to off-load your
 problems to someone else? Do it here, this is your quiet place. Not too long, not too short. Just right.</p>
-</section>
+</section> -->
+
+<?php
+//retrieve and display about page
+//connect
+include("includes/db-connect.php"); 
+
+//prepare
+$stmt = $pdo->prepare("SELECT * FROM `about` WHERE `about`.`id` = 1");
+
+//execute
+$stmt->execute();
+
+//display results
+while($row = $stmt->fetch()) { 
+	?><section id="about"><p><?php    
+	echo($row["text"]);
+	//echo("<br/>");
+	?> 
+
+    </section></p><?php    
+}
+?>
 
 <h2>Don't have a place to vent? <a href="register.php">Register</a></h2>
 <?php
