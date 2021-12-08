cd39e447dea90550bc5e94db81b904abc1b96299
pepe27 <83622952+pepe27@users.noreply.github.com>
2021-12-07 21:24:28 -0500

    put code in edit, but not show post in dashbrd yet

--- dashboard.php
+++ dashboard.php
@@ -1,15 +1,10 @@
 <?php 
 //dashboard.php
-//after login, user can see dashboard/links to favourited enteries
-?>
-
-<?php
-
 include("includes/session-check.php");
-
 include("includes/header.php");
 ?>
 
+<title>Dashboard</title>
 <h1>Your Latest Posts</h1>
 <h2><a href="add-post.php">Add Post</a></h2>
 
--- edit-post.php
+++ edit-post.php
@@ -1,17 +1,15 @@
 <?php 
 //edit-post.php
-?>
-
-<?php
-
 include("includes/session-check.php");
-
 include("includes/header.php");
+$id = $_GET["id"];
 ?>
-<title>Edit Post</title>
-<h1>Change is good</h1>
 
-<section id="addPostForm">
+<title>Edit Post</title>
+<h1>Update record #<?= $id ?></h1>
+<h2>Change is good</h2>
+
+<section id="editPostForm">
 <form method="POST" action="process-edit-post.php">
     <label for="Title">Title</label>
 	<input type="text" name="Title" placeholder="Untitled">
@@ -21,6 +19,45 @@ include("includes/header.php");
 
     <label for="name1">Text</label>
     <textarea name="text" id="text" cols="30" rows="10" placeholder="How's your day going?"></textarea> <br>
+   
+	<button type="submit">Edit</button>
+</form>
+</section>
+
+
+<?php
+//Preview of record
+//(id,etc.... of a specific article)
+
+//connect
+include("includes/db-connect.php"); 
+
+//prepare
+$stmt = $pdo->prepare("SELECT * FROM `immnews-article` 
+	WHERE `id` = $id");
+
+//execute
+$stmt->execute();
+
+//display the record
+$row = $stmt->fetch();
+?>
+
+<section id="editPostForm">
+<form method="POST" action="process-edit-post.php">
+    <input type="hidden" name="id" 
+	value="<?= $row["id"] ?>">
+
+    <label for="title">Title</label>
+    <input type="text" name="title" value="<?= $row["title"] ?>">
+
+    <label for="imageUrl">Image URL</label>
+    <input type="text" name="imageUrl" value="<?= $row["imageUrl"] ?>">
+
+    <!-- <input type="text" name="articleTitle" value="<?= $row["text"] ?>"> -->
+
+    <label for="text">Text</label>
+    <textarea name="text" id="text" cols="30" rows="10" value="<?= $row["text"] ?>"></textarea> <br>
    
 	<button type="submit">Edit</button>
 </form>
